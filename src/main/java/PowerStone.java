39d1666162b6c569033059f6aa98db9b8677b7cf
Tyrell Baker <tybaker1@csumb.edu>
2023-09-20 15:06:26 -0700

    Creates the activate method in Powerstone and tested

--- src/main/java/PowerStone.java
+++ src/main/java/PowerStone.java
@@ -1,11 +1,24 @@
-public class PowerStone implements IStone{
 
+
+public class PowerStone implements IStone{
+  public static final double MULTIPLIER = 150.0;
+  /**
+   *Use the multiplier to increase the person power
+   */
   @Override
   public void activate(Person person) {
-
+    System.out.printf("%s's power == %.2f%n", person, person.getPower());
+    double currentPower = person.getPower();
+    currentPower = currentPower * MULTIPLIER;
+    person.setPower(currentPower);
+    System.out.printf("%s's power is NOW == %.2f%n", person, person.getPower());
 
   }
 
+  /**
+   * use the multiplier to decrease the Person's power
+   * @param person
+   */
   @Override
   public void deactivate(Person person) {
 
